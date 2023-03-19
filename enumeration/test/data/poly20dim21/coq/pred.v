b4eb64aab3fec11242c09a8aeb2c608dfa71d327
Quentin Canu <qcanu17@gmail.com>
2023-03-19 22:30:08 +0100

    data

---
+++ enumeration/test/data/poly20dim21/coq/pred.v
@@ -0,0 +1,3 @@
+From BinReader Require Import BinReader.
+
+LoadData "../../enumeration/test/data/poly20dim21/bin/pred.bin" As pred.
---
+++ enumeration/test/data/poly20dim21/coq/x_I.v
@@ -0,0 +1,3 @@
+From BinReader Require Import BinReader.
+
+LoadData "../../enumeration/test/data/poly20dim21/bin/x_I.bin" As x_I.
