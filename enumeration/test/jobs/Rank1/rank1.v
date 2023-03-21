eec1733fb78da4ed92d26a235b2bd0de1ac9d69a
Quentin Canu <qcanu17@gmail.com>
2023-03-21 09:53:38 +0100

    update of rank1 job file

--- enumeration/test/jobs/Rank1/rank1.v
+++ enumeration/test/jobs/Rank1/rank1.v
@@ -2,4 +2,5 @@ From mathcomp Require Import all_ssreflect.
 From PolyhedraHirsch Require Import rank_1_update.
 From __DATA__ Require Import A b bases idx inv order pred x_I.
 
-Time Compute R1.vertex_certif A b bases pred idx x_I inv order.
+Time Definition main := Eval vm_compute in R1.explore_from_initial A b bases pred idx x_I inv order.
+Time Definition test := Eval vm_compute in R1.bench_old A main.
