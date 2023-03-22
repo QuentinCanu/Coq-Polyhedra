449f230dfa6cd40599d6251692671654cf5ac2c4
Xavier Allamigeon <xavier.allamigeon@inria.fr>
2023-03-22 11:35:38 +0100

    Further experiments.

--- enumeration/test/data/poly20dim21/coq_Rank1/rank1.v
+++ enumeration/test/data/poly20dim21/coq_Rank1/rank1.v
@@ -1,5 +1,10 @@
 From mathcomp Require Import all_ssreflect.
-From PolyhedraHirsch Require Import rank_1_update.
-From POLY20DIM21_DATA Require Import A b bases idx inv order pred x_I.
+Require Import PArray Uint63.
+From PolyhedraHirsch Require Import rank_1_update extra_array.
+From POLY20DIM21_DATA Require Import A b bases idx inv order pred x_I steps.
 
-Time Compute R1.vertex_certif A b bases pred idx x_I inv order.
+Eval vm_compute in PArrayUtils.mk_fun (fun i => order.[i]) steps (default order).
+Time Definition main := Eval vm_compute in R1.explore_from_initial A b bases pred idx x_I inv order steps.
+Definition test := Eval vm_compute in R1.array_to_test main bases order steps.
+(*Time Definition bench1 := Eval vm_compute in R1.bench_old A test.*)
+Time Eval vm_compute in R1.bench_old2 A b test.
