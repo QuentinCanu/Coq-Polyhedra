b997d486e81e4f753c7c293f3149325ecd5292e5
Xavier Allamigeon <xavier.allamigeon@inria.fr>
2023-03-23 08:42:37 +0100

    Various benchmarks with only M.

--- enumeration/test/data/poly20dim21/coq_Rank1/old_certif.v
+++ enumeration/test/data/poly20dim21/coq_Rank1/old_certif.v
@@ -3,6 +3,9 @@ Require Import PArray Uint63.
 From Bignums Require Import BigQ.
 From PolyhedraHirsch Require Import rank_1_update extra_array extra_int.
 From POLY20DIM21_DATA Require Import A b bases idx inv order pred x_I steps.
+Require Import rank1.
 
-Time Definition main := Eval vm_compute in R1.explore_from_initial A b bases pred idx x_I inv order steps.
-Eval vm_compute in IFold.ifold (fun i acc => acc && (isSome main.[order.[i]])) steps true.
+(*Definition test := Eval vm_compute in R1.array_to_test main bases order steps.
+(*Time Definition bench1 := Eval vm_compute in R1.bench_old A test.*)
+Time Eval vm_compute in R1.check_basic_point A b test.
+*)
