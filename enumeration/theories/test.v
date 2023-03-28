6ec179969c4531af09fe4db0636ebba7b37519f2
Quentin Canu <qcanu17@gmail.com>
2023-03-28 14:06:43 +0200

    addition of basic point computations

--- enumeration/theories/test.v
+++ enumeration/theories/test.v
@@ -54,13 +54,28 @@ Definition certif_pred :=[|
 Definition order := [|1;2;3|0|]%uint63.
 Definition steps := length order.
 
-Time Definition main := Eval vm_compute in R1.explore_from_initial A b certif_bases certif_pred idx x inv q order steps.
-Print main.
-Eval vm_compute in R1.vertex_certif A b certif_bases certif_pred idx x inv q order steps.
+(* Compute R1.explore_from_initial A b certif_bases certif_pred idx x inv q order 1. *)
+
+Time Definition init := Eval vm_compute in R1.initial A b certif_bases idx x inv q.
+Time Definition init_main := Eval vm_compute in R1.initial_main A b certif_bases idx x inv q.
+Time Compute let (idx,rs) := certif_pred.[order.[0]] in
+let (r,s) := rs in
+let I := certif_bases.[idx] in
+if init_main.[idx] is Some (x, B, M, q) then
+  let '(x', B', M', q') := R1.update b I r s x B M q in
+  if R1.sat_lex M' q' b certif_bases.[order.[0]] then init_main.[order.[0] <- Some (x', B', M', q')] else init_main
+else init_main.
+(* Let x' := Eval vm_compute in main.1.1.1.
+Let B' := Eval vm_compute in main.1.1.2.
+Let M' := Eval vm_compute in main.1.2.
+Let q' := Eval vm_compute in main.2. *)
+
+(* Compute R1.update b certif_bases.[0] 2 1 x' B' M' q'. *)
+
 
 End Cross2.
 
-Module Cube3.
+(* Module Cube3.
 
 Definition A := [|
   [|1;0;0|0|];
@@ -112,7 +127,7 @@ Definition steps := length order.
 Definition test := Eval vm_compute in R1.vertex_certif A b certif_bases certif_pred idx x inv q order steps.
 Print test.
 
-End Cube3.
+End Cube3. *)
 
 (* From Coq Require Import ExtrOCamlInt63 ExtrOCamlPArray ExtrOcamlBasic Extraction.
 
