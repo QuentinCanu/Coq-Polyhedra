a171cea106a8862e1c031bc33ca0ffeb9ed71ecc
Xavier Allamigeon <xavier.allamigeon@inria.fr>
2023-03-30 09:13:44 +0200

    Back to integer pivoting, check-rank-1-update is in another branch.

--- enumeration/test/data/poly20dim21/coq_Rank1/rank1.v
+++ enumeration/test/data/poly20dim21/coq_Rank1/rank1.v
@@ -1,11 +1,13 @@
-Require Import PArray Uint63.
+Require Import Uint63.
 From mathcomp Require Import all_ssreflect.
 From PolyhedraHirsch Require Import rank_1_update.
 From POLY20DIM21_DATA Require Import A b bases idx inv order pred x_I steps det.
 
 Definition det := R1.get_num det.
-(*Time Definition main := Eval vm_compute in R1.explore_from_initial
-                                       A b bases pred idx x_I inv det order steps.*)
-Time Eval vm_compute in R1.check_all_updates bases pred (R1.explore_from_initial
-                                       A b bases pred idx x_I inv det order steps) order steps.
-(*Time Compute R1.vertex_certif A b bases pred idx x_I inv det order steps.*)
+Eval vm_compute in det.
+Time Compute R1.vertex_certif A b bases pred idx x_I inv det order steps.
+(*Time (*Definition main :=*) Eval vm_compute in R1.explore_from_initial
+                                            A b bases pred idx x_I inv det order steps.
+*)
+(*Time Definition res := Eval vm_compute in R1.num_profile (R1.explore_from_initial
+  A b bases pred idx x_I inv det order steps) order steps.*)
