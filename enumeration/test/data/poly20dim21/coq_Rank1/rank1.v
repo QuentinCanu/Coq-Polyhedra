fcb53d5184a26b28ba1151c6e7a642b2405e9b9c
Quentin Canu <qcanu17@gmail.com>
2023-04-06 13:46:04 +0200

    bin ignored

--- enumeration/test/data/poly20dim21/coq_Rank1/rank1.v
+++
@@ -1,6 +0,0 @@
-From mathcomp Require Import all_ssreflect.
-From PolyhedraHirsch Require Import rank_1_update.
-From POLY20DIM21_DATA Require Import A b bases vtx pred pred_vect idx inv order steps.
-
-Time Compute R1.vertex_certif A b bases vtx pred pred_vect idx inv order steps.
-
