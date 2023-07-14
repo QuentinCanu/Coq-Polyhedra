f94ac0697fb62b7e17c0781009a70f094dd69f83
Quentin Canu <qcanu17@gmail.com>
2023-07-14 15:28:12 +0200

    Updated test folder

--- enumeration/test/scripts/lrs2json.py
+++ enumeration/test/scripts/lrs2json.py
@@ -1,322 +1,264 @@
 #! /usr/bin/env python3
 
-# --------------------------------------------------------------------
-import argparse as argp
-from typing import Tuple
+import json, os
 import fractions as fc
-import itertools as it
-import json
-import networkx as nx
-import os
-import random as rd
-import re
-import subprocess as sp
-import sympy as sym
-import sys
-import time
-import tqdm
+import argparse as argp
+import math, fractions, random as rd
+from . import farkas as fk
+from . import core
 
+import sympy as sym
 from sympy.polys.domains  import QQ
 from sympy.polys.matrices import DomainMatrix
 
-from prerequisite import core, farkas as fk
-
-# --------------------------------------------------------------------
+# Common functions
+# -------------------------------------------------------------------
 def bigq(x):
     return str(x)
 
-# --------------------------------------------------------------------
-# Extract the polyhedron to a json file
-def make_Po(name):
-    input_ine = core.resource(name, "lrs", f"{name}.ine")
-
-    with open(input_ine, 'r') as stream:
+# Extract polyhedron information from lrs files
+# -------------------------------------------------------------------
+def get_polyhedron_from_lrs(name):
+    input = core.resource(name,"lrs",f"{name}.ine")
+    with open(input, 'r') as stream:
         mx = [x.strip() for x in stream]
         mx = [x.split() for x in mx[mx.index('begin')+2:mx.index('end')]]
-        Po_aux = [list(map(fc.Fraction, xs)) for xs in mx]
+        Po = [list(map(fc.Fraction, xs)) for xs in mx]
 
-    m = len(Po_aux)
-    n = len(Po_aux[0]) - 1
-    ine = [([x for x in line[1:]], -line[0]) for line in Po_aux]
-    ine = [(list(map(bigq, p1)), bigq(p2)) for p1,p2 in ine]
-    A,b = zip(*ine)
+    Po = [([x for x in line[1:]], -line[0]) for line in Po]
+    Po = [(list(map(bigq, p1)), bigq(p2)) for p1,p2 in Po]
+    A,b = zip(*Po)
+    return A, b
 
-    return m, n, A, b
-
-# ------------------------------------------------------------------------------------------
-# Extract the vertices data from the .ext file
-def extract_ineqs(line):
-    line, i = line[line.index('facets')+1:], 0
-
-    try:
-        line.remove(":")
-    except ValueError:
-        pass
-
-    while i < len(line):
-        if not re.match('^\d+$', line[i]):
-            break
-        i += 1
-
-    return sorted([int(i) - 1 for i in line[:i]])
-
-def extract_vertices(name):
-    poly_ext = core.resource(name, "lrs", f"{name}.ext")
-    with open(poly_ext, 'r') as stream:
+def get_bases_from_lrs(name):
+    input = core.resource(name,"lrs",f"{name}.ext")
+    with open(input, 'r') as stream:
         mx = [x.strip() for x in stream]
-        mx = [x.split() for x in mx if x[0] in ["V", "1"] and x[1] != "-"]
-    bas_vtx = [(extract_ineqs(mx[i]), mx[i+1][1:]) for i in range(0, len(mx), 2)]
-    res_vtx, res_bas = {}, {}
-    for bas,vtx in bas_vtx:
-        if tuple(vtx) in res_vtx:
-            res_vtx[tuple(vtx)].append(bas)
-        else:
-            res_vtx[tuple(vtx)] = [bas]
-        res_bas[frozenset(bas)] = vtx 
-    return res_vtx, res_bas
+        mx = [x.split() for x in mx[mx.index('begin')+2:mx.index('end')]]
+        mx = [(x[x.index('facets')+1:x.index('det=')-1], x[x.index('det=')+1]) if x[0]!="1" else x[1:] for x in mx]
+        couples = [(mx[i], mx[i+1]) for i in range(0,len(mx),2)]
+        couples = [(((list(map((lambda x : int(x) - 1), b))), d) ,v) for (b,d),v in couples]
+        bas2vtx = {frozenset(b) : v for ((b,_),v) in couples}
+        bas2det = {frozenset(b) : d for ((b,d),_) in couples}
+    return sorted([b for (b,_),_ in couples]), bas2vtx, bas2det
 
-# --------------------------------------------------------------------
-def matrix_of_bas(A, bas):
-    extracted = [A[i] for i in bas]
-    return sym.Matrix(extracted)
-
-# --------------------------------------------------------------------
-def bas_adj(bas1, bas2):
-    return len(set.intersection(set(bas1), set(bas2))) == len(bas1) - 1
-
-# --------------------------------------------------------------------
-def has_bas_adj(l1, l2):
-    for bas1 in l1:
-        for bas2 in l2:
-            if bas_adj(bas1, bas2):
-                return True
-    return False
-
-# --------------------------------------------------------------------
-def inverse_big(M):
+#rewrite A and b to be integer matrices
+# -------------------------------------------------------------------
+def to_gmp_matrix(m):
+    M = sym.Matrix(m)
     c = M.shape
-    M0 = [[QQ(M[i,j].p, M[i,j].q) for j in range(c[0])] for i in range(c[0])]
-    big = DomainMatrix(M0, c, QQ)
-    return big.inv().to_Matrix()
+    M0 = [[QQ(M[i,j].p, M[i,j].q) for j in range(c[1])] for i in range(c[0])]
+    res = DomainMatrix(M0, c, QQ)
+    return res
 
-# --------------------------------------------------------------------
-def perturbated_matrix(x, mask, A, m):
-    A_r = matrix_of_bas(A, mask)
-    M = inverse_big(A_r)
-    M = -M
-    (n,c) = M.shape
-    N = sym.zeros(n, m)
-    for j in range(c):
-        for i in range(n):
-            N[i,mask[j]] = M[i,j]
-    v = sym.Matrix(n,1,x)
-    return N.col_insert(0,v)
-
-# --------------------------------------------------------------------
-def is_feasible(Po, point):
-    for line in Po:
-        A_i = sym.Matrix([line[0]])
-        res_mat = A_i * point
-        res = res_mat.tolist()[0]
-        if res < [fc.Fraction(x) for x in line[1]]:
-            return False
-    return True
-
-# --------------------------------------------------------------------
-def convert_pert_matrix(PM):
+def list_of_gmp_matrix(PM):
+    PM = PM.to_Matrix()
     (p,q)=PM.shape
     res = []
     for j in range(q):
         res.append([bigq(fc.Fraction(PM[k,j].p, PM[k,j].q)) for k in range(p)])
     return res
 
-# --------------------------------------------------------------------
-def make_vertices_lex(dic_bas, m, A):
-    res = []
-    for (p1, p2) in tqdm.tqdm([(list(bas), vtx) for bas,vtx in dic_bas.items()], desc="Extraction of lexicographic feasible basis"):
-        PM = perturbated_matrix(p2, p1, A, m)
-        # if is_feasible(Po, PM):
-        res.append((p1, convert_pert_matrix(PM)))
-    def key_lex(x): return x[0]
-    res.sort(key=key_lex)
-    return res
+def poly_scale(A,b):
+    gmp_A, gmp_b = to_gmp_matrix(A), to_gmp_matrix(b)
+    ca, cb = gmp_A.shape, gmp_b.shape
+    scale = [None for _ in range(ca[0])]
+    for i in range(ca[0]):
+        mult, div = gmp_b[i,0].element.denominator, gmp_b[i,0].element.numerator
+        for j in range(ca[1]):
+            mult = QQ.lcm(mult, gmp_A[i,j].element.denominator)
+            div = QQ.gcd(div, gmp_A[i,j].element.numerator)
+        scale[i] = (mult/div)
+    A, b = gmp_A.to_Matrix(), gmp_b.to_Matrix()
+    res_A, res_b = [], []
+    for i in range(ca[0]):
+        aux_A = []
+        for j in range(ca[1]):
+            aux_A.append(bigq(fc.Fraction(scale[i] * A[i,j])))
+        res_A.append(aux_A)
+        res_b.append(bigq(fc.Fraction(scale[i] *  b[i,0])))
+    return res_A, res_b      
 
-def make_vertices_simpl(dic_vtx):
-    res = list(dic_vtx.keys())
-    def key_simpl(x): return [fc.Fraction(e) for e in x]
-    res.sort(key=key_simpl)
-    return res
-# --------------------------------------------------------------------
-# def make_vertices_simpl(aux):
-#     res = []
-#     def key_simpl(x): return [fc.Fraction(e) for e in x[1]]
-#     aux.sort(key=key_simpl)
-#     last_one = None
-#     for (p1, p2) in tqdm.tqdm(aux, desc="Extraction of vertices and their bases"):
-#         if p2 == last_one:
-#             bases, point = res[-1]
-#             res[-1] = bases + [p1], p2
-#         else:
-#             res += [([p1],p2)]
-#             last_one = p2
-#     return res
+# Compute the initial basing point from which we compute our vertex certification
+# -------------------------------------------------------------------
 
-# --------------------------------------------------------------------
-def make_graph_lex(dic_lex_index, m, n):
-    graph= [[] for _ in dic_lex_index.keys()]
-    for (base, index) in tqdm.tqdm(dic_lex_index.items(), desc="Extraction of the graph lex..."):
-        reg = len(graph[index])
-        for i in base:
-            if reg >= n:
-                break
-            minus_i = base - {i}
-            for j in range(m):
-                if j not in base:
-                    candidate = minus_i | {j}
-                    res = dic_lex_index.get(candidate,None)
-                    if res is not None and index < res:
-                        graph[index] += [res]
-                        graph[res] += [index]
-                        reg += 1
+def get_idx(bases, bas2det):
+    min = math.inf
+    idx = 0
+    for i in range(len(bases)):
+        bas = bases[i]
+        det = fc.Fraction(bas2det[frozenset(bas)])
+        if det < min:
+            min = det
+            idx = i
+    return idx
 
-    return [sorted(elt) for elt in graph]
+def get_initial_basing_point(A,b,base):
+    A_I = [A[i] for i in base]
+    b_I = [b[i] for i in base]
+    gmp_A_I, gmp_b_I = to_gmp_matrix(A_I), to_gmp_matrix(b_I)
+    x_I = gmp_A_I.lu_solve(gmp_b_I)
+    inv = gmp_A_I.inv()
+    det = gmp_A_I.det()
+    return (list_of_gmp_matrix(det * x_I)[0], list_of_gmp_matrix(det * inv), int(det))
 
-# --------------------------------------------------------------------
-def make_morf(dic_lex_index, dic_simpl_index, dic_bas):
-    morf, morf_inv = [None for _ in range(len(dic_lex_index))], [None for _ in range(len(dic_simpl_index))]
-    for base, i in dic_lex_index.items():
-        vtx = dic_bas[base]
-        j = dic_simpl_index[tuple(vtx)]
-        morf[i] = j
-        morf_inv[j] = i
-    return morf, morf_inv
+# Construct the graph of lex feasible bases + order of construction
+# -------------------------------------------------------------------
+def get_lex_graph(bases,idx, m, n):
+    bases_dic = {frozenset(base) : i for (i,base) in enumerate(bases)}
+    graph = [set() for _ in range(len(bases))]
+    order = []
+    pred = [(0,0,0) for _ in range(len(bases))]
+    visited = {i : False for i in bases_dic.keys()}
+    visited[frozenset(bases[idx])] = True
+    queue = [idx]
+    pointer = 0
+    while True:
+        if pointer >= len(queue):
+            break
+        idx_base = queue[pointer]
+        order.append(idx_base)
+        reg = len(graph[idx_base])
+        if reg < n:
+            base = bases[idx_base]
+            base_set = set(base)
+            for s in range(len(bases[idx_base])):
+                for r in range(m):
+                    if r not in base_set:
+                        nei_set = frozenset(base_set - {base[s]} | {r})
+                        if nei_set in bases_dic:
+                            idx_nei = bases_dic[nei_set]
+                            graph[idx_base].add(idx_nei)
+                            if not visited[nei_set]:
+                                visited[nei_set] = True
+                                queue.append(idx_nei)
+                                pred[idx_nei] = (idx_base,r,s)
+        pointer += 1
+    return [sorted(elt) for elt in graph], order[1:], pred
 
-# --------------------------------------------------------------------
-def make_graph_simpl(G_lex, morf, K):
-    graph = [[] for i in range(K)]
-    for i in range(len(G_lex)):
+# Construct the graph of vertices + certificates related to the image graph
+# -------------------------------------------------------------------
+def get_unsrt_vtx(bases,bas2vtx):
+    vtx_list = [None for _ in bases]
+    for i in range(len(bases)):
+        bas = bases[i]
+        vtx = bas2vtx[frozenset(bas)]
+        vtx_list[i] = vtx
+    return vtx_list
+
+def get_vtx(bas2vtx):
+    vtx_list = [i for i in bas2vtx.values()]
+    vtx_list = sorted(set([tuple(map((lambda x : fractions.Fraction(x)), l)) for l in vtx_list]))
+    return [list(map(str,elt)) for elt in vtx_list]
+        
+def get_morph(bases, vtx, bas2vtx):
+    morph, morph_inv = [None for _ in bases], [None for _ in vtx]
+    aux = {tuple(vtx[i]) : i for i in range(len(vtx))}
+    for i in range(len(bases)):
+        bas = bases[i]
+        v = bas2vtx[frozenset(bas)]
+        j = aux[tuple(v)]
+        morph[i] = j
+        morph_inv[j] = i
+    return morph, morph_inv
+
+def get_graph_vtx(graph_lex, morf, length_vtx):
+    graph = [[] for i in range(length_vtx)]
+    for i in range(len(graph_lex)):
         tgt_i = morf[i]
-        for j in G_lex[i]:
+        for j in graph_lex[i]:
             tgt_j = morf[j]
             if tgt_i != tgt_j and tgt_j not in graph[tgt_i]:
                 graph[tgt_i].append(tgt_j)
     return [sorted(x) for x in graph]
 
-        
-
-    # for i in tqdm.tqdm(range(len(vertices)), desc="Extraction of the graph of vertices..."):
-    #     for j in range(len(vertices)):
-    #         if i < j and has_bas_adj(vertices[i][0], vertices[j][0]):
-    #             graph[i] += [j]
-    #             graph[j] += [i]
-    # return graph
-        
-
-    
-
-# --------------------------------------------------------------------
-def make_edge_inv(G_lex, G_simpl, morf):
+def get_edge_inv(G_lex, G_simpl, morf):
     edge_inv = [[None for j in range(len(G_simpl[i]))] for i in range(len(G_simpl))]
-    for i in tqdm.tqdm(range(len(G_lex))):
+    for i in range(len(G_lex)):
         for j in range(len(G_lex[i])):
             nei = G_lex[i][j]
             if morf[i] != morf[nei]:
                 j_ = next(i for i,v in enumerate(G_simpl[morf[i]]) if v == morf[nei])
                 edge_inv[morf[i]][j_] = (i,nei)
     return edge_inv
-
-# --------------------------------------------------------------------
-def make_cert(A, m, n):
-    A = sym.Matrix(A).transpose()
+    
+# Get final certificates (Farkas, dim_full)
+# -------------------------------------------------------------------
+def get_farkas_cert(A, m, n):
+    A = to_gmp_matrix(A).transpose()
     cert_pos, cert_neg = [], []
-    for k in tqdm.tqdm(range(n), desc="Extraction of farkas certificates"):
+    for k in range(n):
         cert_pos.append(list(map(bigq,fk.farkas_gen(A, n, m, k))))
         cert_neg.append(list(map(bigq,fk.farkas_gen(-A, n, m, k))))
     return cert_pos, cert_neg
 
-# --------------------------------------------------------------------
-def make_dim_full(lbl_simpl, n):
+def get_dim_full(vtx, n):
     while True:
-        map_lbl = rd.sample(range(len(lbl_simpl)), n+1)
+        map_lbl = rd.sample(range(len(vtx)), n+1)
         map_lbl.sort()
-        M = sym.Matrix([list(map(fc.Fraction, lbl_simpl[i])) for i in list(map_lbl)[1:]])
-        N = sym.Matrix([list(map(fc.Fraction, lbl_simpl[map_lbl[0]])) for _ in range(n)])
+        M = sym.Matrix([list(map(fc.Fraction, vtx[i])) for i in list(map_lbl)[1:]])
+        N = sym.Matrix([list(map(fc.Fraction, vtx[map_lbl[0]])) for _ in range(n)])
         Q = M - N
-        Q_det = Q.det()
+        Q_gmp = to_gmp_matrix(Q)
+        Q_det = Q_gmp.det()
         if Q_det != 0:
-            Q_inv = inverse_big(Q)
-            Q_res = [[bigq(Q_inv[i,j]) for i in range(n)] for j in range(n)]
+            Q_inv = Q.gmp_inv()
+            Q_res = list_of_gmp_matrix(Q_inv)
             return list(map_lbl)[0], list(map_lbl)[:1], Q_res
 
-# --------------------------------------------------------------------
-def make_start(G, thre):
-    g = nx.Graph({i : G[i] for i in range(len(G))})
-    for i in tqdm.tqdm(range(len(G))):
-        L = nx.eccentricity(g,v=[i])
-        dist = [i for i in L.values()][0]
-        if dist > thre:
-            return i
 
-# --------------------------------------------------------------------
+# Main function, write a json file with one certificate per entry
+# -------------------------------------------------------------------
 def optparser():
-    parser = argp.ArgumentParser(
-      description='Extract json data from polyhedron data')
+    parser = argp.ArgumentParser(description='Extract json data from polyhedron data')
     parser.add_argument('name', help="the name of the polyhedron to be extracted")
-    parser.add_argument('--hirsch',
-          help="Additional data extraction if it is a counterexample to the hirsch conjecture",
-          action="store_true")
     return parser
 
-# --------------------------------------------------------------------
-def main():
-    args   = optparser().parse_args()
-    name   = args.name
-    hirsch = args.hirsch
+# -------------------------------------------------------------------
+def lrs2dict(name):
 
-    # jsondir = resource(name, 'json')
+    # Compute everything
+    A,b = get_polyhedron_from_lrs(name)
+    A,b = poly_scale(A,b)
+    bases, bas2vtx, bas2det = get_bases_from_lrs(name)
+    idx = get_idx(bases, bas2det)
+    x_I, inv, det = (get_initial_basing_point(A,b,bases[idx]))
+    m,n = len(A), len(A[0])
+    graph_lex, order, pred = get_lex_graph(bases,idx,m,n)
+    vtx = get_unsrt_vtx(bases, bas2vtx)
+    # morph, morph_inv = get_morph(bases,vtx,bas2vtx)
+    # graph_vtx = get_graph_vtx(graph_lex,morph,len(vtx))
+    # edge_inv = get_edge_inv(graph_lex,graph_vtx,morph)
+    # farkas_cert_pos, farkas_cert_neg = get_farkas_cert(A,m,n)
 
-    # if os.path.exists(jsondir):
-    #     print(f'remove {jsondir} first', file=sys.stderr)
-    #     exit(1)
 
-    # os.makedirs(jsondir)
+    # Store in a dictionnary
 
     tgtjson = {}
+    tgtjson['A'] = A
+    tgtjson['b'] = b
+    tgtjson['bases'] = bases
+    tgtjson['idx'] = idx
+    tgtjson['x_I'] = x_I
+    tgtjson['inv'] = inv
+    tgtjson['det'] = det
+    tgtjson['order'] = order
+    tgtjson['steps'] = len(order)
+    tgtjson['pred'] = pred
+    tgtjson['vtx'] = vtx
+    return tgtjson
 
-    m, n, A, b = make_Po(name)
-    tgtjson["Po"] = (A,b)
-    dic_vtx, dic_bas= extract_vertices(name)
-    lbl_lex = make_vertices_lex(dic_bas, m, A)
-    tgtjson["lbl_lex"] = lbl_lex
-    lbl_simpl = make_vertices_simpl(dic_vtx)
-    tgtjson["lbl_simpl"] = lbl_simpl
-    dic_lex_index = {frozenset(lbl_lex[i][0]) : i for i in range(len(lbl_lex))}
-    dic_simpl_index = {tuple(lbl_simpl[i]) : i for i in range(len(lbl_simpl))}
-    G_lex = make_graph_lex(dic_lex_index, m, n)
-    tgtjson["G_lex"] = G_lex
-    morf, morf_inv = make_morf(dic_lex_index, dic_simpl_index, dic_bas)
-    tgtjson["morf"] = morf
-    tgtjson["morf_inv"] = morf_inv
-    G_simpl = make_graph_simpl(G_lex, morf, len(dic_vtx))
-    tgtjson["G_simpl"] = G_simpl
-    edge_inv = make_edge_inv(G_lex, G_simpl, morf)
-    tgtjson["edge_inv"] = edge_inv
-    cert_pos, cert_neg = make_cert(A, m, n)
-    tgtjson["cert_pos"] = cert_pos
-    tgtjson["cert_neg"] = cert_neg
-    if hirsch:
-        origin, map_lbl, inv_lbl = make_dim_full(lbl_simpl, n)
-        tgtjson["origin"] = origin
-        tgtjson["map_lbl"] = map_lbl
-        tgtjson["inv_lbl"] = inv_lbl
-        start = make_start(G_simpl,m-n)
-        tgtjson["start"] = start
-
+def dict2json(name,tgtdict):
     tgtdir = core.resource(name)
     
     with open(os.path.join(tgtdir, f"{name}.json"), "w") as stream:
-        json.dump(tgtjson,stream, indent=2)
+        json.dump(tgtdict,stream, indent=2)
 
-# --------------------------------------------------------------------
+def main(name):
+    dict2json(name,lrs2dict(name))
+
+# -------------------------------------------------------------------
 if __name__ == '__main__':
-    main()
+    args   = optparser().parse_args()
+    name   = args.name
+    main(name)
